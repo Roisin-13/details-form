f2f64cd07fb79be57b4e6ce0de0a3f4c18f7fe6f
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-09-14 17:58:48 +0100

    project started - form added for details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+const [cardNumber, setCardNumber] = useState("");
+const [name, setName] = useState("");
+
+const handleCardNumber = (e) => {
+  e.preventDefault();
+  setCardNumber(e.target.value);
+  console.log(cardNumber);
+}
+
+const handleName = (e) => {
+  e.preventDefault();
+  setName(e.target.value);
+  console.log(name);
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <form>
+        <label>"card number"
+        <input type="text" placeholder="card number"  onChange={handleCardNumber} value={cardNumber}></input>
+        </label>
+        
+        <label>"card holder name"
+        <input type="text" placeholder="card holder name" onChange={handleName} value={name}></input>
+        </label>
+
+            <div className="putInCard">
+              <p>{cardNumber}</p>
+              <p>{name}</p>
+            </div>
+      </form>
+
     </div>
   );
 }
