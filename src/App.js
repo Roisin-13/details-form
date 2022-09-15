b731058f1ec1f29de882bdf5fd2ee84fada9cffe
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-09-15 15:26:20 +0100

    form functionality added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {useState} from 'react';
 function App() {
 const [cardNumber, setCardNumber] = useState("");
 const [name, setName] = useState("");
+//const [cardDetails, setCardDetails] = useState([]);
+const [cardDetails, setCardDetails] = useState({cardNumber:"", name:""});
 
 const handleCardNumber = (e) => {
   e.preventDefault();
@@ -18,6 +20,35 @@ const handleName = (e) => {
   console.log(name);
 }
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+ const validateForm = () => {
+  let number = document.getElementById("number").value;
+  let name = document.getElementById("name").value;
+  if (number == "" || name =="") {
+    alert("form must be filled out");
+    return false;
+  } else {
+    document.getElementById("justAForm").style.display = "none";
+    // name = "";
+    // number = "";
+    document.getElementById("btn").style.display = "block";
+    setCardDetails(card => ({
+      ...card,
+      cardNumber:cardNumber, name:name
+   }));
+
+  }
+}
+validateForm();
+}
+
+const handleClick = (e) => {
+  // document.getElementById("justAForm").style.display = "block";
+  // document.getElementById("number").value = "";
+  // document.getElementById("name").value = "";
+  window.location.reload(false);
+}
 
   return (
     <div className="App">
@@ -27,21 +58,25 @@ const handleName = (e) => {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
-      <form>
+      <form onSubmit={handleSubmit} id="justAForm">
         <label>"card number"
-        <input type="text" placeholder="card number"  onChange={handleCardNumber} value={cardNumber}></input>
-        </label>
-        
-        <label>"card holder name"
-        <input type="text" placeholder="card holder name" onChange={handleName} value={name}></input>
+        <input type="text" placeholder="card number"  onChange={handleCardNumber} pattern="[0-9]{5}" value={cardNumber} id="number"></input>
         </label>
 
+        <label>"card holder name"
+        <input type="text" placeholder="card holder name" onChange={handleName} value={name} pattern="[A-Za-z ]{3,}" id="name"></input>
+        </label>
+<button type="submit">enter details</button>
             <div className="putInCard">
               <p>{cardNumber}</p>
               <p>{name}</p>
             </div>
       </form>
-
+<div id="newCard">
+  <p>{cardDetails.cardNumber}</p>
+  <p>{cardDetails.name}</p>
+  <button style={{display: "none"}} id="btn" onClick={handleClick}>start again</button>
+</div>
     </div>
   );
 }
