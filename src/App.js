b52a82557f95a92bb54ff01a82c675acf1887fd7
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-09-16 16:19:23 +0100

    functionality and styling finished - only responsive styling left

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 //import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
+import {ReactComponent as Logo} from './images/card-logo.svg';
+import {ReactComponent as Complete} from './images/icon-complete.svg';
+
+const validateForm = (name, number, cvv) => {
+  if (number == "" || name =="" || cvv =="") {
+    return false;
+  }
+    return true;
+  
+}
 
 function App() {
 const [cardNumber, setCardNumber] = useState("");
 const [name, setName] = useState("");
+const [cvv, setCvv] = useState("");
 //const [cardDetails, setCardDetails] = useState([]);
-const [cardDetails, setCardDetails] = useState({cardNumber:"", name:""});
+const [cardDetails, setCardDetails] = useState({cardNumber:"", name:"", cvv:""});
 
 const handleCardNumber = (e) => {
   e.preventDefault();
@@ -20,63 +31,81 @@ const handleName = (e) => {
   console.log(name);
 }
 
+const handleCvv = (e) => {
+  e.preventDefault();
+  setCvv(e.target.value);
+  console.log(cvv);
+}
+
 const handleSubmit = (e) => {
   e.preventDefault();
- const validateForm = () => {
-  let number = document.getElementById("number").value;
-  let name = document.getElementById("name").value;
-  if (number == "" || name =="") {
-    alert("form must be filled out");
-    return false;
-  } else {
+let number = document.getElementById("number").value;
+let name = document.getElementById("name").value;
+let cvv = document.getElementById("cvv").value;
+const isValid = validateForm(name, number, cvv);
+  if (!isValid) {
+    alert("form must be filled out"); 
+  }
+  if (isValid) {
     document.getElementById("justAForm").style.display = "none";
-    // name = "";
-    // number = "";
+    document.getElementById("formComplete").style.display = "block";
     document.getElementById("btn").style.display = "block";
+    document.getElementById("newCard").style.display = "block";
+    document.getElementById("newCardBack").style.display = "block";
     setCardDetails(card => ({
       ...card,
-      cardNumber:cardNumber, name:name
-   }));
-
+      cardNumber:cardNumber, name:name, cvv:cvv
+    }));
   }
-}
-validateForm();
 }
 
 const handleClick = (e) => {
-  // document.getElementById("justAForm").style.display = "block";
-  // document.getElementById("number").value = "";
-  // document.getElementById("name").value = "";
   window.location.reload(false);
 }
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
       <form onSubmit={handleSubmit} id="justAForm">
-        <label>"card number"
-        <input type="text" placeholder="card number"  onChange={handleCardNumber} pattern="[0-9]{5}" value={cardNumber} id="number"></input>
-        </label>
-
-        <label>"card holder name"
-        <input type="text" placeholder="card holder name" onChange={handleName} value={name} pattern="[A-Za-z ]{3,}" id="name"></input>
-        </label>
-<button type="submit">enter details</button>
-            <div className="putInCard">
-              <p>{cardNumber}</p>
-              <p>{name}</p>
-            </div>
+          <div className="formCont">
+            <label>card number
+              <input type="text" placeholder="e.g. 1234 5678"  onChange={handleCardNumber} pattern="[0-9]{8}" value={cardNumber} id="number"></input>
+            </label>
+            <label>card holder name 
+              <input type="text" placeholder="e.g. Jane Appleseed" onChange={handleName} value={name} pattern="[A-Za-z ]{3,}" id="name"></input>
+            </label>
+            <label>cvc
+              <input type="text" placeholder="e.g. 123" onChange={handleCvv} value={cvv} pattern="[0-9]{3}" id="cvv"></input>
+            </label>
+            <button type="submit">Continue</button>
+          </div>
+          <div className="putInCardBack">
+            <p id="cvvDiv">{cvv}</p>
+          </div>
+          <div className="putInCard">
+            <Logo id="logo"/>
+            <p className="carNumDiv">{cardNumber}</p>
+            <p className="nameDiv">{name}</p>
+          </div>
       </form>
-<div id="newCard">
-  <p>{cardDetails.cardNumber}</p>
-  <p>{cardDetails.name}</p>
-  <button style={{display: "none"}} id="btn" onClick={handleClick}>start again</button>
-</div>
+
+      <div id="newCard" style={{display: "none"}}>
+      <Logo id="logo"/>
+        <p className="carNumDiv">{cardDetails.cardNumber}</p>
+        <p className="nameDiv">{cardDetails.name}</p>
+      </div>
+
+      <div id="newCardBack"  style={{display: "none"}}>
+            <p id="cvvDiv">{cardDetails.cvv}</p>
+      </div>
+
+      <div style={{display: "none"}} id="formComplete">
+        <Complete id ="complete"/>
+        <h2>THANK YOU</h2>
+        <p>We've added your card details</p>
+        <button style={{display: "none"}} id="btn" onClick={handleClick}>Continue</button>
+      </div>
+
+      <div className="bigBackground"></div>
     </div>
   );
 }
